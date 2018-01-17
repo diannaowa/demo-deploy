ca65b29077b84d50694b70c3ce2bcbe31bb48315
Your Name <you@example.com>
2018-01-17 17:33:28 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+COM=$1
+NAMESPACE=$2
+TAG=$3
+
+function load_template {
+
+  data=$(cat $1.json)
+  data_for=$(echo $data | sed 's/\"/\\\"/g')
+  eval echo $data_for > tmp/$1.json
+
+  return 0
+}
+
+load_template $COM
+load_template ${COM}-svc
+
+kubectl --server $K8S_MASTER_HOST apply -f tmp/$COM.json
+kubectl --server $K8S_MASTER_HOST apply -f tmp/${COM}-svc.json
